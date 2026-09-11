199281cf2412f95c3df1ad4e3ec00f6c3f34151b
Manha Cetafaju <Manha.Cetafaju@labfies.org.br>
2026-09-11 08:37:37 -0300

    feat(documento):simulacao do banco de dados

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -6,6 +6,10 @@ namespace BancoSENAIAPI.Controllers
     [Route("api/v1/[controller]")]
     public class DocumentoController : Controller
     {
-        
+        private readonly string _caminhoRaiz = Path.Combine(Directory.GetCurrentDirectory(), "clienteArquivos");
+
+        private static List<Models.documentoMETADADO> _documentosMetadados = new List<Models.documentoMETADADO>();
+
+        private static int _nextId = 1;
     }
 }
