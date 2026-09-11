11f6174d8ef5189fbb706def2eec6c4e0f90903e
Manha Cetafaju <Manha.Cetafaju@labfies.org.br>
2026-09-11 08:26:22 -0300

    feat: Adicionando documentoMETADADO e controller

---
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Models/documentoMETADADO.cs
@@ -0,0 +1,12 @@
+﻿namespace BancoSENAIAPI.Models
+{
+    public class documentoMETADADO
+    {
+        public int Id { get; set; }
+        public string nome { get; set; }
+        public string extensao { get; set; }
+        public string caminho {  get; set; }
+        public int CodCliente { get; set; }
+
+    }
+}
